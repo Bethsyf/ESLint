29bb2a762c6e08d5b49f0f3c021824763bb971af
Bethsyf <bethsyfalcon@gmail.com>
2022-07-22 10:26:31 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'double'],
+  },
+};
---
+++ app.js
@@ -0,0 +1,10 @@
+const nombre = 'Bethsy'
+
+const persona = {nombre}
+
+console.log(persona)
+
+const saludar = (fNombre) => {
+    console.log(`Hola! ¿que tal, ${fNombre}`);
+};
+const persona = { nombre: 'Otra persona' }
